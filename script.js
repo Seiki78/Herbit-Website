311c9042ca2cbe486a7b899e84f579031c648b44
Seiki <Seikii78@gmail.com>
2024-09-25 00:27:04 +0700

    Fix Script

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+document.addEventListener('DOMContentLoaded', function() {
+	// เอา 'hidden' ออก เพื่อให้ alert แสดง
+	var alerts = document.querySelectorAll('.alert');
+	alerts.forEach(function(alert) {
+		alert.classList.remove('hidden');
+	});
 
+	// ตั้งเวลาซ่อนหลังแสดง
+	setTimeout(function() {
+		alerts.forEach(function(alert) {
+			alert.classList.add('hidden');
+		});
+	}, 3000); // ปิด flash messages หลัง 3 วินาที
+});
+
+// ตรวจสอบว่ามี flash message ประเภท 'danger' หรือไม่ แล้วเปิด modal(pop login) อัตโนมัติ
+const hasFlashDanger = "{{ 'danger' in get_flashed_messages(with_categories=true) }}";
+	if (hasFlashDanger === 'True') {
+		modal.classList.add('active');
+		overlay.classList.add('active');
+	}
+
+//
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
 allSideMenu.forEach(item=> {
 	const li = item.parentElement;
 
@@ -11,9 +33,6 @@ allSideMenu.forEach(item=> {
 	})
 });
 
-
-
-
 // TOGGLE SIDEBAR
 const menuBar = document.querySelector('#content nav .bx.bx-menu');
 const sidebar = document.getElementById('sidebar');
@@ -22,12 +41,7 @@ menuBar.addEventListener('click', function () {
 	sidebar.classList.toggle('hide');
 })
 
-
-
-
-
-
-
+//
 const searchButton = document.querySelector('#content nav form .form-input button');
 const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
 const searchForm = document.querySelector('#content nav form');
@@ -44,17 +58,12 @@ searchButton.addEventListener('click', function (e) {
 	}
 })
 
-
-
-
-
 if(window.innerWidth < 768) {
 	sidebar.classList.add('hide');
 } else if(window.innerWidth > 576) {
 	searchButtonIcon.classList.replace('bx-x', 'bx-search');
 	searchForm.classList.remove('show');
 }
-
 
 window.addEventListener('resize', function () {
 	if(this.innerWidth > 576) {
@@ -63,10 +72,8 @@ window.addEventListener('resize', function () {
 	}
 })
 
-
-
+//
 const switchMode = document.getElementById('switch-mode');
-
 switchMode.addEventListener('change', function () {
 	if(this.checked) {
 		document.body.classList.add('dark');
