01a042ea7405f7e962eff091f23cafccc4db876b
Seiki <Seikii78@gmail.com>
2024-09-25 06:54:20 +0700

    Fix Script Fix css

--- static/js/script.js
+++ static/js/script.js
@@ -13,26 +13,31 @@ document.addEventListener('DOMContentLoaded', function() {
 	}, 3000); // ปิด flash messages หลัง 3 วินาที
 });
 
-//
+//sidebar
 const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
-allSideMenu.forEach(item=> {
-	const li = item.parentElement;
+if (allSideMenu.length > 0) {
+    allSideMenu.forEach(item => {
+        const li = item.parentElement;
 
-	item.addEventListener('click', function () {
-		allSideMenu.forEach(i=> {
-			i.parentElement.classList.remove('active');
-		})
-		li.classList.add('active');
-	})
-});
+        item.addEventListener('click', function () {
+            allSideMenu.forEach(i => {
+                i.parentElement.classList.remove('active');
+            });
+            li.classList.add('active');
+        });
+    });
+}
 
 // TOGGLE SIDEBAR
 const menuBar = document.querySelector('#content nav .bx.bx-menu');
 const sidebar = document.getElementById('sidebar');
 
-menuBar.addEventListener('click', function () {
-	sidebar.classList.toggle('hide');
-})
+if (menuBar && sidebar) {  // ตรวจสอบว่า element มีอยู่จริงก่อนเรียกใช้งาน
+    menuBar.addEventListener('click', function () {
+        sidebar.classList.toggle('hide');
+    });
+}
+
 
 //ค้นหา
 const searchButton = document.querySelector('#content nav form .form-input button');
