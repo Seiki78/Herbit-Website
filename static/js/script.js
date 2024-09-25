c9138a7752b35bc262e98c218b9496f5c097f4cd
Seiki <Seikii78@gmail.com>
2024-09-25 12:18:08 +0700

    Fix Script index and webkit css

--- static/js/script.js
+++ static/js/script.js
@@ -33,29 +33,4 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
     
-    // Search form
-    const searchButton = document.querySelector('#content nav form .form-input button');
-    const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
-    const searchForm = document.querySelector('#content nav form');
-    if (searchButton && searchForm && searchButtonIcon) {
-        searchButton.addEventListener('click', function (e) {
-            if (window.innerWidth < 576) {
-                e.preventDefault();
-                searchForm.classList.toggle('show');
-                if (searchForm.classList.contains('show')) {
-                    searchButtonIcon.classList.replace('bx-search', 'bx-x');
-                } else {
-                    searchButtonIcon.classList.replace('bx-x', 'bx-search');
-                }
-            }
-        });
-    }
-
-    // Resize event for sidebar
-    window.addEventListener('resize', function () {
-        if (window.innerWidth > 576) {
-            searchButtonIcon.classList.replace('bx-x', 'bx-search');
-            searchForm.classList.remove('show');
-        }
-    });
 });
