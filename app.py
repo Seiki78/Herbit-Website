5265508a4e159d0d12d49c0cb1314b6e20b982af
Seiki <Seikii78@gmail.com>
2024-10-22 19:10:14 +0700

    fix value input/ select_herbal เลือกมาแบบเจาะจง

--- app.py
+++ app.py
@@ -338,81 +338,81 @@ def delete_trains(train_id):
 def edit_trains(train_id):
     if request.method == 'POST':
         Age = request.form['Age']
-        pregnant = request.form['pregnant']
-        dizziness1 = request.form.get('dizziness1')
-        Palpitations = request.form.get('Palpitations')
-        squeamish = request.form.get('squeamish')
-        vomit = request.form.get('vomit')
-        dizziness2 = request.form.get('dizziness2')
-        dizziness3 = request.form.get('dizziness3')
-        dizziness4 = request.form.get('dizziness4')
-        colic1 = request.form.get('colic1')
-        tired = request.form.get('tired')
-        cannot_sleep = request.form.get('cannot_sleep')
-        flatulence = request.form.get('flatulence')
-        stomach_ache = request.form.get('stomach_ache')
-        constipation1 = request.form.get('constipation1')
-        diarrhea1 = request.form.get('diarrhea1')
-        hemorrhoids = request.form.get('hemorrhoids')
-        menstruation = request.form.get('menstruation')
-        Menstrual_cramps = request.form.get('Menstrual_cramps')
-        postpartum_woman = request.form.get('postpartum_woman')
-        Lochia = request.form.get('Lochia')
-        Vaginal_Discharge = request.form.get('Vaginal_Discharge')
-        Nourish_blood = request.form.get('Nourish_blood')
-        fever1 = request.form.get('fever1')
-        inner_heat = request.form.get('inner_heat')
-        Measles = request.form.get('Measles')
-        Chickenpox = request.form.get('Chickenpox')
-        fever2 = request.form.get('fever2')
-        fever3 = request.form.get('fever3')
-        cough = request.form.get('cough')
-        phlegm = request.form.get('phlegm')
-        cold = request.form.get('cold')
-        Allergic_Rhinitis = request.form.get('Allergic_Rhinitis')
-        body_aches = request.form.get('body_aches')
-        tendon = request.form.get('tendon')
-        Tight_numb = request.form.get('Tight_numb')
-        muscles_tendons = request.form.get('muscles_tendons')
-        dizziness5 = request.form.get('dizziness5')
+        pregnant = 1 if request.form.get('pregnant') else 0
+        dizziness1 = 1 if request.form.get('dizziness1') else 0
+        Palpitations = 1 if request.form.get('Palpitations') else 0
+        squeamish = 1 if request.form.get('squeamish') else 0
+        vomit = 1 if request.form.get('vomit') else 0
+        dizziness2 = 1 if request.form.get('dizziness2') else 0
+        dizziness3 = 1 if request.form.get('dizziness3') else 0
+        dizziness4 = 1 if request.form.get('dizziness4') else 0
+        colic1 = 1 if request.form.get('colic1') else 0
+        tired = 1 if request.form.get('tired') else 0
+        cannot_sleep = 1 if request.form.get('cannot_sleep') else 0
+        flatulence = 1 if request.form.get('flatulence') else 0
+        stomach_ache = 1 if request.form.get('stomach_ache') else 0
+        constipation1 = 1 if request.form.get('constipation1') else 0
+        diarrhea1 = 1 if request.form.get('diarrhea1') else 0
+        hemorrhoids = 1 if request.form.get('hemorrhoids') else 0
+        menstruation = 1 if request.form.get('menstruation') else 0
+        Menstrual_cramps = 1 if request.form.get('Menstrual_cramps') else 0
+        postpartum_woman = 1 if request.form.get('postpartum_woman') else 0
+        Lochia = 1 if request.form.get('Lochia') else 0
+        Vaginal_Discharge = 1 if request.form.get('Vaginal_Discharge') else 0
+        Nourish_blood = 1 if request.form.get('Nourish_blood') else 0
+        fever1 = 1 if request.form.get('fever1') else 0
+        inner_heat = 1 if request.form.get('inner_heat') else 0
+        Measles = 1 if request.form.get('Measles') else 0
+        Chickenpox = 1 if request.form.get('Chickenpox') else 0
+        fever2 = 1 if request.form.get('fever2') else 0
+        fever3 = 1 if request.form.get('fever3') else 0
+        cough = 1 if request.form.get('cough') else 0
+        phlegm = 1 if request.form.get('phlegm') else 0
+        cold = 1 if request.form.get('cold') else 0
+        Allergic_Rhinitis = 1 if request.form.get('Allergic_Rhinitis') else 0
+        body_aches = 1 if request.form.get('body_aches') else 0
+        tendon = 1 if request.form.get('tendon') else 0
+        Tight_numb = 1 if request.form.get('Tight_numb') else 0
+        muscles_tendons = 1 if request.form.get('muscles_tendons') else 0
+        dizziness5 = 1 if request.form.get('dizziness5') else 0
 
-        pregnant = 0 if pregnant is None else 1
-        dizziness1 = 0 if dizziness1 is None else 1
-        Palpitations = 0 if Palpitations is None else 1
-        squeamish = 0 if squeamish is None else 1
-        vomit = 0 if vomit is None else 1
-        dizziness2 = 0 if dizziness2 is None else 1
-        dizziness3 = 0 if dizziness3 is None else 1
-        dizziness4 = 0 if dizziness4 is None else 1
-        colic1 = 0 if colic1 is None else 1
-        tired = 0 if tired is None else 1
-        cannot_sleep = 0 if cannot_sleep is None else 1
-        flatulence = 0 if flatulence is None else 1
-        stomach_ache = 0 if stomach_ache is None else 1
-        constipation1 = 0 if constipation1 is None else 1
-        diarrhea1 = 0 if diarrhea1 is None else 1
-        hemorrhoids = 0 if hemorrhoids is None else 1
-        menstruation = 0 if menstruation is None else 1
-        Menstrual_cramps = 0 if Menstrual_cramps is None else 1
-        postpartum_woman = 0 if postpartum_woman is None else 1
-        Lochia = 0 if Lochia is None else 1
-        Vaginal_Discharge = 0 if Vaginal_Discharge is None else 1
-        Nourish_blood = 0 if Nourish_blood is None else 1
-        fever1 = 0 if fever1 is None else 1
-        inner_heat = 0 if inner_heat is None else 1
-        Measles = 0 if Measles is None else 1
-        Chickenpox = 0 if Chickenpox is None else 1
-        fever2 = 0 if fever2 is None else 1
-        fever3 = 0 if fever3 is None else 1
-        cough = 0 if cough is None else 1
-        phlegm = 0 if phlegm is None else 1
-        cold = 0 if cold is None else 1
-        Allergic_Rhinitis = 0 if Allergic_Rhinitis is None else 1
-        body_aches = 0 if body_aches is None else 1
-        tendon = 0 if tendon is None else 1
-        Tight_numb = 0 if Tight_numb is None else 1
-        muscles_tendons = 0 if muscles_tendons is None else 1
-        dizziness5 = 0 if dizziness5 is None else 1
+        # pregnant = 0 if pregnant is None else 1
+        # dizziness1 = 0 if dizziness1 is None else 1
+        # Palpitations = 0 if Palpitations is None else 1
+        # squeamish = 0 if squeamish is None else 1
+        # vomit = 0 if vomit is None else 1
+        # dizziness2 = 0 if dizziness2 is None else 1
+        # dizziness3 = 0 if dizziness3 is None else 1
+        # dizziness4 = 0 if dizziness4 is None else 1
+        # colic1 = 0 if colic1 is None else 1
+        # tired = 0 if tired is None else 1
+        # cannot_sleep = 0 if cannot_sleep is None else 1
+        # flatulence = 0 if flatulence is None else 1
+        # stomach_ache = 0 if stomach_ache is None else 1
+        # constipation1 = 0 if constipation1 is None else 1
+        # diarrhea1 = 0 if diarrhea1 is None else 1
+        # hemorrhoids = 0 if hemorrhoids is None else 1
+        # menstruation = 0 if menstruation is None else 1
+        # Menstrual_cramps = 0 if Menstrual_cramps is None else 1
+        # postpartum_woman = 0 if postpartum_woman is None else 1
+        # Lochia = 0 if Lochia is None else 1
+        # Vaginal_Discharge = 0 if Vaginal_Discharge is None else 1
+        # Nourish_blood = 0 if Nourish_blood is None else 1
+        # fever1 = 0 if fever1 is None else 1
+        # inner_heat = 0 if inner_heat is None else 1
+        # Measles = 0 if Measles is None else 1
+        # Chickenpox = 0 if Chickenpox is None else 1
+        # fever2 = 0 if fever2 is None else 1
+        # fever3 = 0 if fever3 is None else 1
+        # cough = 0 if cough is None else 1
+        # phlegm = 0 if phlegm is None else 1
+        # cold = 0 if cold is None else 1
+        # Allergic_Rhinitis = 0 if Allergic_Rhinitis is None else 1
+        # body_aches = 0 if body_aches is None else 1
+        # tendon = 0 if tendon is None else 1
+        # Tight_numb = 0 if Tight_numb is None else 1
+        # muscles_tendons = 0 if muscles_tendons is None else 1
+        # dizziness5 = 0 if dizziness5 is None else 1
         
         # อัปเดตข้อมูลทุกอย่างใน MongoDB
         trains_collection.update_one({'_id': ObjectId(train_id)}, {'$set': {'Age': Age, 'pregnant': pregnant, 'dizziness1': dizziness1, 'Palpitations': Palpitations,
@@ -433,10 +433,10 @@ def edit_trains(train_id):
     # ดึงข้อมูล trains ที่ต้องการแก้ไข
     trains = trains_collection.find_one({'_id': ObjectId(train_id)})
 
-    # ดึงข้อมูล collection herbals_data
-    herbals = herbals_data_collection.find()
+    # ดึง hm_id จาก train นั้นๆ และหา herbal ที่เกี่ยวข้อง
+    selected_herbal = herbals_data_collection.find_one({'hm_id': trains['hm_id']})
     
-    return render_template('edit_trains.html', trains=trains, herbals=herbals)
+    return render_template('edit_trains.html', trains=trains, selected_herbal=selected_herbal)
 
 if __name__ == '__main__':
 
